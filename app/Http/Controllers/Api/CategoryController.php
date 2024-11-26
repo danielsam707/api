87acea3fc8759ae138f899593496c18abd636272
Daniel Arias <danielsam707@gmail.com>
2024-11-26 16:59:25 -0500

    Clase 9: Consistencia ente endpoints-Recursos y coleciones |Se cran dos clases CategoryResource y CategoryColletion para aislar la estrutura y composicion de un recurso y decidir que es lo que queremos entregar como respuesta, decidimos que campos mostrar o no

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,23 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+use App\Http\Resources\CategoryCollection;
 
 use Illuminate\Http\Request;
+
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return Category::all();
+        //return CategoryResource::collection(Category::all()); esta seria la manera si no hubiera un CategoryColletion
+        return new CategoryCollection(Category::all());
+
     }
 
     public function show(Category $category)
     {
-        // Carga la categoria pero con todas las recetas y se debe hacer la relacion en el modelo Category.
-        /**
-         * Normalmente se utliza with en vez de load, pero el with se utiliza cuando la consulta es
-         * desde cero, en este caso ya hay una consulta anterior que me arrojo el valor de $category
-         */
-        return $category->load('recipes');
+        $category = $category->load('recipes');
+        return new CategoryResource($category);
     }
 }
